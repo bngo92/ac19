dcc2d923636ddd4e10324857a6fee37f7130f9c7
Bryan Ngo <bngo92@gmail.com>
2019-12-02 22:05:36 -0800

    Day 3

--- src/bin/d2.rs
+++ src/bin/d2.rs
@@ -4,24 +4,59 @@ fn main() {
     let args: Vec<_> = env::args().collect();
     let s: Vec<_> = "1,12,2,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,13,27,1,10,27,31,2,31,13,35,1,10,35,39,2,9,39,43,2,43,9,47,1,6,47,51,1,10,51,55,2,55,13,59,1,59,10,63,2,63,13,67,2,67,9,71,1,6,71,75,2,75,9,79,1,79,5,83,2,83,13,87,1,9,87,91,1,13,91,95,1,2,95,99,1,99,6,0,99,2,14,0,0".to_owned().split(',').map(|s| s.parse::<i32>().unwrap()).collect();
     if args[1] == "p1" {
-        println!("{:?}", d2("1,0,0,0,99".to_owned().split(',').map(|s| s.parse::<i32>().unwrap()).collect()));
-        println!("{:?}", d2("2,3,0,3,99".to_owned().split(',').map(|s| s.parse::<i32>().unwrap()).collect()));
-        println!("{:?}", d2("2,4,4,5,99,0".to_owned().split(',').map(|s| s.parse::<i32>().unwrap()).collect()));
-        println!("{:?}", d2("1,1,1,4,99,5,6,0,99".to_owned().split(',').map(|s| s.parse::<i32>().unwrap()).collect()));
-        println!("{:?}", d2("1,9,10,3,2,3,11,0,99,30,40,50".to_owned().split(',').map(|s| s.parse::<i32>().unwrap()).collect()));
+        println!(
+            "{:?}",
+            d2("1,0,0,0,99"
+                .to_owned()
+                .split(',')
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect())
+        );
+        println!(
+            "{:?}",
+            d2("2,3,0,3,99"
+                .to_owned()
+                .split(',')
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect())
+        );
+        println!(
+            "{:?}",
+            d2("2,4,4,5,99,0"
+                .to_owned()
+                .split(',')
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect())
+        );
+        println!(
+            "{:?}",
+            d2("1,1,1,4,99,5,6,0,99"
+                .to_owned()
+                .split(',')
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect())
+        );
+        println!(
+            "{:?}",
+            d2("1,9,10,3,2,3,11,0,99,30,40,50"
+                .to_owned()
+                .split(',')
+                .map(|s| s.parse::<i32>().unwrap())
+                .collect())
+        );
         println!("{:?}", d2("1,0,0,3,1,1,2,3,1,3,4,3,1,5,0,3,2,10,1,19,1,19,9,23,1,23,13,27,1,10,27,31,2,31,13,35,1,10,35,39,2,9,39,43,2,43,9,47,1,6,47,51,1,10,51,55,2,55,13,59,1,59,10,63,2,63,13,67,2,67,9,71,1,6,71,75,2,75,9,79,1,79,5,83,2,83,13,87,1,9,87,91,1,13,91,95,1,2,95,99,1,99,6,0,99,2,14,0,0".to_owned().split(',').map(|s| s.parse::<i32>().unwrap()).collect()));
         println!("{:?}", d2(s));
     } else if args[1] == "p2" {
         for noun in 0..=99 {
-           for verb in 0..=99 {
-               let mut copy = s.clone();
-               copy[1] = noun;
-               copy[2] = verb;
-               if d2(copy)[0] == 19690720 {
-                   println!("{}", 100 * noun + verb);
-                   break;
-               }
-           }
+            for verb in 0..=99 {
+                let mut copy = s.clone();
+                copy[1] = noun;
+                copy[2] = verb;
+                if d2(copy)[0] == 19690720 {
+                    println!("{}", 100 * noun + verb);
+                    break;
+                }
+            }
         }
     }
 }
@@ -36,13 +71,13 @@ fn d2(input: Vec<i32>) -> Vec<i32> {
     input
 }
 
-fn d2_next(input: &Vec<i32>, position: usize) -> Option<Vec<i32>> {
+fn d2_next(input: &[i32], position: usize) -> Option<Vec<i32>> {
     match input[position] {
         1 => {
             let x = input[position + 1] as usize;
             let y = input[position + 2] as usize;
             let z = input[position + 3] as usize;
-            let mut copy = input.clone();
+            let mut copy = input.to_vec();
             copy[z] = input[x] + input[y];
             Some(copy)
         }
@@ -50,11 +85,11 @@ fn d2_next(input: &Vec<i32>, position: usize) -> Option<Vec<i32>> {
             let x = input[position + 1] as usize;
             let y = input[position + 2] as usize;
             let z = input[position + 3] as usize;
-            let mut copy = input.clone();
+            let mut copy = input.to_vec();
             copy[z] = input[x] * input[y];
             Some(copy)
         }
         99 => None,
-        _ => panic!()
+        _ => panic!(),
     }
 }
