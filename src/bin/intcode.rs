d206040b5f5c1c4c6782b49d939b277638db32fd
Bryan Ngo <bngo92@gmail.com>
2019-12-07 20:51:48 -0800

    Refactor intcode for day 7

--- src/bin/intcode.rs
+++ src/bin/intcode.rs
@@ -1,147 +1,115 @@
 #![feature(slice_patterns)]
 
+use std::collections::VecDeque;
 use std::iter;
 
 fn main() {}
 
-pub fn run_intcode(program: Vec<i32>, input: i32) -> Vec<i32> {
-    iter::successors(Some((program, 0)), |(program, ip)| {
-        process_opcode(&program, *ip, input)
-    })
-    .last()
-    .unwrap()
-    .0
+pub fn run_intcode(program: Vec<i32>, mut input: VecDeque<i32>) -> (Vec<i32>, VecDeque<i32>) {
+    let mut output = VecDeque::new();
+    (
+        iter::successors(Some((program, 0)), |(program, ip)| {
+            match process_opcode(&program, *ip, &mut input, &mut output) {
+                OpcodeResult::Next(t) => Some(t),
+                OpcodeResult::Halt => None,
+                _ => panic!(),
+            }
+        })
+        .last()
+        .unwrap()
+        .0,
+        output,
+    )
 }
 
-fn process_opcode(program: &Vec<i32>, ip: usize, input: i32) -> Option<(Vec<i32>, usize)> {
+pub enum OpcodeResult {
+    Next((Vec<i32>, usize)),
+    Halt,
+    Blocked,
+}
+
+pub fn process_opcode(
+    program: &Vec<i32>,
+    ip: usize,
+    input: &mut VecDeque<i32>,
+    output: &mut VecDeque<i32>,
+) -> OpcodeResult {
+    //println!("{:?}", program);
     let opcode = format!("{:05}", program[ip]);
+    //println!("{}, {:?}", ip, opcode);
     let mut iter = opcode.chars();
     let _a = iter.next().unwrap();
     let b = iter.next().unwrap();
     let c = iter.next().unwrap();
     match &opcode.as_bytes()[3..5] {
         b"01" => {
-            let x = if c == '0' {
-                program[program[ip + 1] as usize]
-            } else {
-                program[ip + 1]
-            };
-            let y = if b == '0' {
-                program[program[ip + 2] as usize]
-            } else {
-                program[ip + 2]
-            };
+            let x = get_value(program, ip + 1, c);
+            let y = get_value(program, ip + 2, b);
             let z = program[ip + 3] as usize;
-            let mut copy = program.clone();
-            copy[z] = x + y;
-            Some((copy, ip + 4))
+            let next = [&program[..z], &program[z + 1..]].join(&(x + y));
+            OpcodeResult::Next((next, ip + 4))
         }
         b"02" => {
-            let x = if c == '0' {
-                program[program[ip + 1] as usize]
-            } else {
-                program[ip + 1]
-            };
-            let y = if b == '0' {
-                program[program[ip + 2] as usize]
-            } else {
-                program[ip + 2]
-            };
+            let x = get_value(program, ip + 1, c);
+            let y = get_value(program, ip + 2, b);
             let z = program[ip + 3] as usize;
-            let mut copy = program.clone();
-            copy[z] = x * y;
-            Some((copy, ip + 4))
+            let next = [&program[..z], &program[z + 1..]].join(&(x * y));
+            OpcodeResult::Next((next, ip + 4))
         }
-        b"03" => {
-            let x = program[ip + 1] as usize;
-            let mut copy = program.clone();
-            copy[x] = input;
-            Some((copy, ip + 2))
-        }
+        b"03" => match input.pop_front() {
+            Some(i) => {
+                let x = program[ip + 1] as usize;
+                let next = [&program[..x], &program[x + 1..]].join(&i);
+                OpcodeResult::Next((next, ip + 2))
+            }
+            None => OpcodeResult::Blocked,
+        },
         b"04" => {
-            let x = if c == '0' {
-                program[program[ip + 1] as usize]
-            } else {
-                program[ip + 1]
-            };
-            println!("{}", x);
-            Some((program.clone(), ip + 2))
+            let x = get_value(program, ip + 1, c);
+            output.push_back(x);
+            OpcodeResult::Next((program.clone(), ip + 2))
         }
         b"05" => {
-            let x = if c == '0' {
-                program[program[ip + 1] as usize]
-            } else {
-                program[ip + 1]
-            };
+            let x = get_value(program, ip + 1, c);
             if x != 0 {
-                let y = if b == '0' {
-                    program[program[ip + 2] as usize]
-                } else {
-                    program[ip + 2]
-                } as usize;
-                Some((program.clone(), y))
+                let y = get_value(program, ip + 2, b) as usize;
+                OpcodeResult::Next((program.clone(), y))
             } else {
-                Some((program.clone(), ip + 3))
+                OpcodeResult::Next((program.clone(), ip + 3))
             }
         }
         b"06" => {
-            let x = if c == '0' {
-                program[program[ip + 1] as usize]
-            } else {
-                program[ip + 1]
-            };
+            let x = get_value(program, ip + 1, c);
             if x == 0 {
-                let y = if b == '0' {
-                    program[program[ip + 2] as usize]
-                } else {
-                    program[ip + 2]
-                } as usize;
-                Some((program.clone(), y))
+                let y = get_value(program, ip + 2, b) as usize;
+                OpcodeResult::Next((program.clone(), y))
             } else {
-                Some((program.clone(), ip + 3))
+                OpcodeResult::Next((program.clone(), ip + 3))
             }
         }
         b"07" => {
-            let x = if c == '0' {
-                program[program[ip + 1] as usize]
-            } else {
-                program[ip + 1]
-            };
-            let y = if b == '0' {
-                program[program[ip + 2] as usize]
-            } else {
-                program[ip + 2]
-            };
+            let x = get_value(program, ip + 1, c);
+            let y = get_value(program, ip + 2, b);
             let z = program[ip + 3] as usize;
-            let mut copy = program.clone();
-            if x < y {
-                copy[z] = 1;
-            } else {
-                copy[z] = 0;
-            }
-            Some((copy, ip + 4))
+            let next = [&program[..z], &program[z + 1..]].join(&((x < y) as i32));
+            OpcodeResult::Next((next, ip + 4))
         }
         b"08" => {
-            let x = if c == '0' {
-                program[program[ip + 1] as usize]
-            } else {
-                program[ip + 1]
-            };
-            let y = if b == '0' {
-                program[program[ip + 2] as usize]
-            } else {
-                program[ip + 2]
-            };
+            let x = get_value(program, ip + 1, c);
+            let y = get_value(program, ip + 2, b);
             let z = program[ip + 3] as usize;
-            let mut copy = program.clone();
-            if x == y {
-                copy[z] = 1;
-            } else {
-                copy[z] = 0;
-            }
-            Some((copy, ip + 4))
+            let next = [&program[..z], &program[z + 1..]].join(&((x == y) as i32));
+            OpcodeResult::Next((next, ip + 4))
         }
-        b"99" => None,
+        b"99" => OpcodeResult::Halt,
         _ => panic!(),
+    }
+}
+
+fn get_value(program: &Vec<i32>, ip: usize, mode: char) -> i32 {
+    if mode == '0' {
+        program[program[ip] as usize]
+    } else {
+        program[ip]
     }
 }
